2394b11b7e69fa7407fd414bacf47b9d92228a7d
cplover <cplover@live.com>
2015-07-27 14:39:34 +0800

    Initial Commit

---
+++ Bahamut/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bahamut
+//
+//  Created by AlexChow on 15/7/27.
+//  Copyright (c) 2015年 GStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
