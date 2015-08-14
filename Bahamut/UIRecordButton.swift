56e71d9a2168c9d311178872bbb164f42bd24446
cplover <cplover@live.com>
2015-08-14 12:20:42 +0800

    add PBJVision to pod

---
+++ Bahamut/UIRecordButton.swift
@@ -0,0 +1,21 @@
+//
+//  UIRecordButton.swift
+//  Bahamut
+//
+//  Created by AlexChow on 15/8/14.
+//  Copyright © 2015年 GStudio. All rights reserved.
+//
+
+import UIKit
+
+class UIRecordButton: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- Podfile
+++ Podfile
@@ -4,3 +4,5 @@ use_frameworks!
 pod 'Alamofire', :git => 'https://github.com/zhouguangjie/Alamofire.git', :branch => 'swift-2.0'
 
 pod 'EVReflection', :git => 'https://github.com/zhouguangjie/EVReflection.git', :branch => 'Swift2'
+
+pod 'PBJVision'
