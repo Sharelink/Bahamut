f4767eff9ea47eb115be1e71f221b109b3818ca1
cplover <cplover@live.com>
2016-01-27 15:23:35 +0800

    update kernel

--- SharelinkKernel/makeKernel.sh
+++ SharelinkKernel/makeKernel.sh
@@ -1,2 +1,2 @@
-xcodebuild -workspace ../Sharelink.xcworkspace -scheme SharelinkKernel -sdk iphoneos -destination 'platform=iOS,name=generic' build
-xcodebuild -workspace ../Sharelink.xcworkspace -scheme SharelinkKernel -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6' build
+xcodebuild -workspace ../Sharelink.xcworkspace -scheme SharelinkKernel -sdk iphoneos -destination 'platform=iOS,name=generic' clean build
+xcodebuild -workspace ../Sharelink.xcworkspace -scheme SharelinkKernel -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6' clean build
